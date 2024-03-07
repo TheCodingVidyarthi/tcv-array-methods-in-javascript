229aa8be2d84bfa685394f52bda8ec571942093d
thecodingvidyarthi <thecodingvidyarthi@gmail.com>
2024-03-07 20:08:59 +0530

    introduction to array

---
+++ 1_introduction_to_array.js
@@ -0,0 +1,22 @@
+/**
+ * Array in JavaScript
+ * An array is a data structure that stores  collection of items
+ */
+
+/**
+ * To define an array we use opening and closing square bracket `[]`
+ * Since there are no items present inside the bracket it is an empty array
+ */
+let emptyArray = [];
+
+/**
+ * Defining an array and assigning values.
+ * In the below example the array has 4 items
+ */
+let fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
+
+/**
+ * An array containing mixed datatypes
+ * In the below example the array contains number, String, boolean, object, array
+ */
+let mixedArray = [1, 'Hello', true, { key: 'value' }, [5, 10]];
